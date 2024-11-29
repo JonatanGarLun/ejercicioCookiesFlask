394375b76cacf994d9051acdada222e4efc9d513
JonatanGarLun <jgarlun2112@fp.iesromerovargas.com>
2024-11-29 12:12:08 +0100

    Todo funcional, solo falta decoración y añadir archivo md con las respuestas a las preguntas

--- app.py
+++ app.py
@@ -2,6 +2,16 @@ from flask import Flask, make_response, request, render_template, url_for, redir
 
 app = Flask(__name__)
 
+@app.route('/set_cookie')
+def set_cookie():
+    response = make_response("Cookie creada")
+    response.set_cookie('usuario', 'Jonatan', max_age=3600, httponly=True, secure=True)
+    return redirect(url_for('home'))
+
+@app.route('/home')
+def home():
+    return render_template('index.html')
+
 @app.route('/')
 def index():
     consent = request.cookies.get('cookies_consent')
@@ -15,11 +25,18 @@ def privacy_policy():
 def consent():
     if request.method == 'POST':
         option = request.form['option']
-        response = make_response(redirect(url_for('get_cookies')))
+        response = make_response(redirect(url_for('set_cookie')))
         if option == 'accept_all':
             response.set_cookie('usuario', 'Jonatan')
-        return url_for('set_cookie', option=option)
+            return redirect(url_for('set_cookie'))
+        else:
+            return redirect(url_for('reject'))
     return render_template('consent.html')
+
+@app.route ('/reject')
+def reject():
+    return render_template('rechazo_cookies.html')
+
 
 if __name__ == '__main__':
     app.run()
